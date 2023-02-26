f38af9e927b86c68ff013056c43247cdef803a14
kimzeze <fridaynight@kakao.com>
2023-02-27 00:01:11 +0900

    textarea, select, 저장버튼 완성

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,10 +1,26 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 const DiaryEditor = () => {
   const [state, setState] = useState({
     author: "",
     content: "",
+    emotion: 1,
   });
+
+  const handleChangeState = (e) => {
+    console.log(e.target.name);
+    console.log(e.target.value);
+
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = () => {
+    console.log(state);
+    alert("저장 성공 ");
+  };
 
   return (
     <div className="DiaryEditor">
@@ -13,18 +29,31 @@ const DiaryEditor = () => {
         <input
           name="author"
           value={state.author}
-          onChange={(e) => {
-            setState({ ...state, author: e.target.value });
-          }}
+          onChange={handleChangeState}
         />
       </div>
       <div>
         <textarea
+          name="content"
           value={state.content}
-          onChange={(e) => {
-            setState({ ...state, content: e.target.value });
-          }}
+          onChange={handleChangeState}
         ></textarea>
+      </div>
+      <div>
+        <select
+          name="emotion"
+          value={state.emotion}
+          onChange={handleChangeState}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
       </div>
     </div>
   );
