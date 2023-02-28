9f38aa2b4f949fb679541591728fdca60274926f
kimzeze <fridaynight@kakao.com>
2023-02-28 17:06:21 +0900

    React 배열 1- 리스트 랜더링(조회)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+import React, { useState, useRef } from "react";
+
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "김도현",
+    content: "안녕1",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "제제김",
+    content: "안녕2",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "미노이",
+    content: "안녕3",
+    emotion: 4,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
