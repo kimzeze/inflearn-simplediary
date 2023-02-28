23c56ec17a95cd8596866c426ce9271d0e5cc40e
kimzeze <fridaynight@kakao.com>
2023-02-28 15:20:15 +0900

    useRef 사용하여 focus 하기

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 const DiaryEditor = () => {
+  const authorInput = useRef();
+  const contentInput = useRef();
+
   const [state, setState] = useState({
     author: "",
     content: "",
@@ -18,7 +21,17 @@ const DiaryEditor = () => {
   };
 
   const handleSubmit = () => {
-    console.log(state);
+    if (state.author.length < 1) {
+      // focus -> useRef를 사용하여 접근함. current.focus()
+      authorInput.current.focus();
+
+      return;
+    }
+    if (state.content.length < 5) {
+      // focus -> useRef를 사용하여 접근함. current.focus()
+      contentInput.current.focus();
+      return;
+    }
     alert("저장 성공 ");
   };
 
@@ -27,6 +40,7 @@ const DiaryEditor = () => {
       <h2>오늘의 일기</h2>
       <div>
         <input
+          ref={authorInput}
           name="author"
           value={state.author}
           onChange={handleChangeState}
@@ -34,6 +48,7 @@ const DiaryEditor = () => {
       </div>
       <div>
         <textarea
+          ref={contentInput}
           name="content"
           value={state.content}
           onChange={handleChangeState}
