efe17a5c97d80bd0573fb91c504e160f63b85954
kimzeze <fridaynight@kakao.com>
2023-03-07 00:00:00 +0900

    onDelete 함수명 onRemove로 변경

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
     setData([newItem, ...data]);
   };
 
-  const onDelete = (targetId) => {
+  const onRemove = (targetId) => {
     console.log(`${targetId}가 삭제되었습니다.`);
     const newDiaryList = data.filter((it) => it.id !== targetId);
     setData(newDiaryList);
@@ -57,7 +57,7 @@ function App() {
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
-      <DiaryList onDelete={onDelete} diaryList={data} />
+      <DiaryList onRemove={onRemove} diaryList={data} />
     </div>
   );
 }
