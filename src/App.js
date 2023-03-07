f7cf3a250768c5a6941f3dd943e5af412229efb9
kimzeze <fridaynight@kakao.com>
2023-03-07 16:53:06 +0900

    api 호출 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,40 +1,39 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import Lifecycle from "./Lifecycle";
+// import Lifecycle from "./Lifecycle";
 
-/* 
-const dummyList = [
-  {
-    id: 1,
-    author: "김도현",
-    content: "안녕1",
-    emotion: 2,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "제제김",
-    content: "안녕2",
-    emotion: 3,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "미노이",
-    content: "안녕3",
-    emotion: 4,
-    created_date: new Date().getTime(),
-  },
-];
- */
+// https://jsonplaceholder.typicode.com/posts/1/comments
 
 function App() {
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/posts/1/comments"
+    ).then((res) => res.json());
+    console.log(res);
+
+    const initData = res.slice(0, 5).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_data: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_data = new Date().getTime();
@@ -65,7 +64,7 @@ function App() {
 
   return (
     <div className="App">
-      <Lifecycle />
+      {/* <Lifecycle /> */}
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
